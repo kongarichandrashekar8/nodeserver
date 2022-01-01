525906ddfde354041cefee377ef4b41fd01fedf8
sai kumar <kongarichandrashekar8@gmail.com>
2022-01-01 15:22:01 +0530

    make it better

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ const { v4: uuidv4 } = require('uuid');
 const PORT =process.env.PORT || 5000
 
 const app = express();
-app.use(cors);
+//app.use(cors);
 const server = app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
